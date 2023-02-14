912319d142566d42a11c06978b3b7e77affbc660
GidJoe <winterm@bfwob.de>
2023-02-14 16:29:19 +0100

    Initial commit

--- src/Bruch.java
+++ src/Bruch.java
@@ -1,27 +1,83 @@
 public class Bruch {
-
     private int zaehler, nenner;
 
-    public Bruch (int numOne, int numTwo) {
+    public Bruch(int numOne, int numTwo) {
         zaehler = numOne;
         nenner = numTwo;
-    }
-
-    public Bruch add(Bruch bruch) {
-        this.zaehler = (this.zaehler * bruch.nenner)+(this.nenner * bruch.zaehler);
-        this.nenner = this.nenner * bruch.nenner;
-
-        this.zaehler =
-        return this;
-
     }
 
     public String toString() {
         return zaehler + " / " + nenner;
     }
+    // Konstruktor?
 
+    // Methode zum Addieren
+    public Bruch add(Bruch bruch) {
+        this.zaehler = (this.zaehler * bruch.nenner) + (this.nenner * bruch.zaehler);
+        this.nenner = this.nenner * bruch.nenner;
+        int teiler = ggt(this.zaehler, this.nenner);
+        this.zaehler = this.zaehler/teiler;
+        this.nenner = this.nenner/teiler;
 
-    public Bruch mul(Bruch anderer) {
-        return new Bruch(zaehler * anderer.zaehler, nenner * anderer.nenner);
+        return this;
+
     }
+
+    // Methode zum Subtrahieren
+    public Bruch subtrahieren(Bruch bruch) {
+        this.zaehler = (this.zaehler * bruch.nenner) - (this.nenner * bruch.zaehler);
+        this.nenner = this.nenner * bruch.nenner;
+        int teiler = ggt(this.zaehler, this.nenner);
+        this.zaehler = this.zaehler/teiler;
+        this.nenner = this.nenner/teiler;
+
+
+        return this;
+
+    }
+
+    // Methode zum Multiplizieren
+    public Bruch multiplizieren(Bruch bruch) {
+        this.zaehler *= bruch.zaehler;
+        this.nenner *= bruch.nenner;
+
+        int teiler = ggt(this.zaehler, this.nenner);
+        this.zaehler = this.zaehler/teiler;
+        this.nenner = this.nenner/teiler;
+
+        return this;
+    }
+
+
+    // Methode zum Dividieren
+    public Bruch dividieren(Bruch bruch) {
+        this.zaehler *= bruch.nenner;
+        this.nenner *= bruch.zaehler;
+        int teiler = ggt(this.zaehler, this.nenner);
+        this.zaehler = this.zaehler/teiler;
+        this.nenner = this.nenner/teiler;
+
+        return this;
+
+    }
+
+
+    public int ggt(int a, int b) {
+
+        if (a == 0)
+            return b;
+        while (b != 0) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+
+        return a;
+    }
+
 }
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,12 @@ public class Main {
         Bruch bruch1 = new Bruch(5,10);
         Bruch bruch2 = new Bruch(10,5);
 
-        System.out.println(bruch1.add(bruch2).toString());
+        System.out.println(bruch1.multiplizieren(bruch2).toString());
+
+        bruch1.add(bruch2);
+
+
+
 
 
     }
